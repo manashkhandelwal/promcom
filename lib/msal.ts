7e444be92f2180282f91fe56821bc5cf7564a5f0
akshat_saini <akshat12.saini@gmail.com>
2026-02-03 00:06:22 +0530

    local host sort

--- lib/msal.ts
+++ lib/msal.ts
@@ -8,8 +8,8 @@ const msalConfig: Configuration = {
   auth: {
     clientId: process.env.NEXT_PUBLIC_AZURE_CLIENT_ID as string,
     authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_TENANT_ID}`,
-    redirectUri: "http://localhost:3000/auth/callback",
-    postLogoutRedirectUri: "http://localhost:3000/",
+    redirectUri: "https://promcom-udgl.vercel.app/auth/callback",
+    postLogoutRedirectUri: "https://promcom-udgl.vercel.app",
   },
   cache: {
     cacheLocation: BrowserCacheLocation.LocalStorage,
